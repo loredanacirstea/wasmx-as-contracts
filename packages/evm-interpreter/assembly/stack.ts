5d0923c9ce6240d2263932ff3f2a3271778f7f34
Loredana Cirstea <loredana.cirstea@gmail.com>
2023-05-30 13:42:49 +0300

    more efficient stack implementation

--- packages/evm-interpreter/assembly/stack.ts
+++ packages/evm-interpreter/assembly/stack.ts
@@ -2,50 +2,51 @@ import { BigInt } from "./bn";
 import { STACK_UNDERFLOW } from "./error";
 
 export class Stack {
-    stack: Array<BigInt> = new Array<BigInt>(2048);
+    stack: StaticArray<BigInt> = new StaticArray<BigInt>(2048);
+    len: i32 = 0;
 
     get(index: u32): BigInt {
         return this.stack[index];
     }
 
     last(): BigInt {
-        return this.stack[this.stack.length - 1];
+        return this.stack[this.len - 1];
     }
 
     length(): i32 {
-        return this.stack.length;
+        return this.len;
     }
 
     push(value: BigInt): void {
-        this.stack.push(value);
+        this.stack[this.len] = value;
+        this.len += 1;
     }
 
     pop(): BigInt {
-        if (this.stack.length === 0) {
+        if (this.len === 0) {
             throw new Error(STACK_UNDERFLOW);
         }
-        return this.stack.pop();
+        const v = this.stack[this.len - 1];
+        this.len -= 1;
+        return v;
     }
 
     dup(x: i32): BigInt {
-        if (x > this.stack.length) throw new Error(`Invalid DUP${x} ; stack length: ${this.stack.length}`);
-        const value = this.stack[this.stack.length - x].copy();
-        this.stack.push(value);
+        if (x > this.len) throw new Error(`Invalid DUP${x} ; stack length: ${this.len}`);
+        const value = this.stack[this.len - x].copy();
+        this.push(value);
         return value;
     }
 
     swap(x: i32): void {
-        if (x >= this.stack.length) {
-            throw new Error(`Invalid SWAP${x} ; stack length: ${this.stack.length}`);
+        if (x >= this.len) {
+            throw new Error(`Invalid SWAP${x} ; stack length: ${this.len}`);
         }
-        const index = this.stack.length - x - 1;
-        const last = this.pop();
+        const index = this.len - x - 1;
+        const last = this.stack[this.len - 1];
         const middle = this.stack[index];
-        const pre = this.stack.slice(0, index);
-        const post = this.stack.slice(index + 1);
-        pre.push(last);
-        this.stack = pre.concat(post);
-        this.stack.push(middle);
+        this.stack[index] = last;
+        this.stack[this.len - 1] = middle;
     }
 }
 
