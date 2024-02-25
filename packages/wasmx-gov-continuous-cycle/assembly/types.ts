3c39aad40fb8da7328a0130360ac9207d75640cf
Loredana Cirstea <loredana.cirstea@gmail.com>
2024-02-25 19:23:08 +0100

    initiate wasmx-gov-continuous-cycle

---
+++ packages/wasmx-gov-continuous-cycle/assembly/types.ts
@@ -0,0 +1,8 @@
+import { JSON } from "json-as/assembly";
+import { Base64String, Bech32String, Coin } from 'wasmx-env/assembly/types';
+import { BigInt } from "wasmx-env/assembly/bn"
+import * as gov from "wasmx-gov/assembly/types"
+
+// export const MODULE_NAME = "gov-continuous"
+export const MODULE_NAME = "gov"
+export const VERSION = "0.0.1"
